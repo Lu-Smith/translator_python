7f00e6a814baebf4ba38f4882cb1c15ec8cf0d11
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-27 20:51:38 +0000

    Added button

--- translator.py
+++ translator.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QWidget, QApplication, QVBoxLayout, QTextEdit,
-                             QLabel, QComboBox)
+                             QLabel, QComboBox, QPushButton)
 from deep_translator import GoogleTranslator
 
 class Translator(QWidget):
@@ -17,19 +17,28 @@ class Translator(QWidget):
     # layout
     layout = QVBoxLayout()
     
-    #input
+    # input
     self.inputText = QTextEdit(self)
     self.inputText.setPlaceholderText("Enter text to translate...")
     layout.addWidget(QLabel("Input Text:"))
     layout.addWidget(self.inputText)
     
-    # Language selection
+    # language selection
     self.languageComboBox = QComboBox(self)
     self.languageComboBox.addItems(["en", "es", "fr", "de", "it"])  
     layout.addWidget(QLabel("Select Language:"))
     layout.addWidget(self.languageComboBox)
     
+    # button
+    self.translateButton = QPushButton("Translate", self)
+    self.translateButton.clicked.connect(self.translate_text)
+    layout.addWidget(self.translateButton)
+
+    
     self.setLayout(layout)
+    
+  def translate_text(self):
+    text = self.inputText.toPlainText
     
 def main():
   app = QApplication(sys.argv)
