2cfa889e8dad32f12dfa15bf130ce75d57fe0bae
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-27 20:47:06 +0000

    Added inputText

--- translator.py
+++ translator.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import (QWidget, QApplication, QVBoxLayout, QTextEdit,
+                             QLabel)
 from deep_translator import GoogleTranslator
 
 class Translator(QWidget):
@@ -12,7 +13,16 @@ class Translator(QWidget):
   def initUI(self):
     self.setWindowTitle("Translator App")
     self.setGeometry(100, 100, 400, 300)
-
+    
+    # layout
+    layout = QVBoxLayout()
+    
+    #input
+    self.inputText = QTextEdit(self)
+    self.inputText.setPlaceholderText("Enter text to translate...")
+    layout.addWidget(QLabel("Input Text:"))
+    layout.addWidget(self.inputText)
+    
 def main():
   app = QApplication(sys.argv)
   translator = Translator()
